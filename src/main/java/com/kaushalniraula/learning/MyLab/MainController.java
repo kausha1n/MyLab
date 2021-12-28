886f03b81797a885e573ce31fb1f9c0d101a6e6c
Kaushal Niraula <kaushal.niraula@gmail.com>
2021-12-27 19:50:55 -0500

    small changes

--- src/main/java/com/kaushalniraula/learning/MyLab/MainController.java
+++ src/main/java/com/kaushalniraula/learning/MyLab/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("greet")
     public String greet(){
-        return "Hi!";
+        return "Hello!";
     }
 
     
